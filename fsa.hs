5e19a965cf8c4c2f361f2fa54fec02253321b8d7
poonkeegan <poonkeegan@gmail.com>
2016-12-03 03:28:56 -0500

    Changed the hardcoded fsa to one that accepts strings over {1, 2, 3}* that have an odd number of at least 1 of the symbols

--- fsa.hs
+++ fsa.hs
@@ -3,7 +3,35 @@ main = do
     let loop = do 
         putStrLn "What string would you to check in the language"
         input <- getLine
-        putStrLn(if fsa input [(0, '0', 0), (0, '1', 1), (1, '0', 1), (1, '1', 0)] [1] 0 then "TRUE" else "FALSE")
+        putStrLn(if fsa input [
+            (1, '1', 4),
+            (1, '2', 7), 
+            (1, '3', 3), 
+            (2, '1', 3),
+            (2, '2', 9), 
+            (2, '3', 4), 
+            (3, '1', 2),
+            (3, '2', 8), 
+            (3, '3', 5), 
+            (4, '1', 5),
+            (4, '2', 6), 
+            (4, '3', 2), 
+            (5, '1', 4),
+            (5, '2', 7), 
+            (5, '3', 3), 
+            (6, '1', 7),
+            (6, '2', 4), 
+            (6, '3', 9), 
+            (7, '1', 6),
+            (7, '2', 5), 
+            (7, '3', 8),
+            (8, '1', 9),
+            (8, '2', 3), 
+            (8, '3', 7), 
+            (9, '1', 8),
+            (9, '2', 2), 
+            (9, '3', 6)] 
+            [2,3,4,6,7,8,9] 1 then "TRUE" else "FALSE")
         loop
     loop
 
@@ -31,8 +59,8 @@ fsa x d f s =
             let filterCond = \y -> fst3 y == s && snd3 y == x !! 0
 
             -- Generate arguments to pass to FSA
-            in let newFsa = fsa (tail x) d f
-            in let getState = \y -> thd3 y
+            in  let newFsa = fsa (tail x) d f
+                    getState = \y -> thd3 y
             in let moveStates = map getState (filter filterCond d)
             in let leadToEnd = map newFsa moveStates
             in elem True leadToEnd
